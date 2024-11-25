9d188e7e08be6d2d5e22e1d2c32908e134d042e8
m-fotso <fotso@usc.edu>
2024-11-25 01:04:34 -0800

    login xml

---
+++ app/src/main/java/com/example/csci310project2treehole/AnonymousTest.java
@@ -0,0 +1,48 @@
+package com.example.csci310project2treehole;
+import java.util.Vector;
+public class AnonymousTest {
+    public void testInvalidPost() {
+        Post post1 = new Post("id", "author", "name", "", "content", 0L, true);
+        Post post2 = new Post("id", "author", "name", "title", "", 0L, true);
+        Post post3 = new Post("id", "author", "name", "", "", 0L, true);
+        assertEquals(post1);
+        assertEquals(post2);
+        assertEquals(post3);
+    }
+    public void testAnonymousNameNoChange() {
+        Post post1 = new Post("id", "author", "name", "title", "content", 0L, true);
+        Post post2 = new Post("id", "author", "name", "title", "content", 0L, true);
+        Post post3 = new Post("id", "author", "name", "title", "content", 0L, true);
+        Vector<String> list = new Vector<String>();
+        String name1 = post1.getAnonymousNameForUser("user1");
+        String name2 = post2.getAnonymousNameForUser("user1");
+        String name3 = post3.getAnonymousNameForUser("user1");
+        list.add(name1);
+        list.add(name2);
+        list.add(name3);
+        assertEquals(list, "Anonymous 1");
+    }
+
+    public void testAnonymousReply() {
+        Post post = new Post("id", "author", "name", "", "content", 0L, true);
+        Reply reply = new Reply("id1", "id","author1", "content", 0L, true, "id", "author");
+        String name = reply.getAnonymousName();
+        assertEquals(name, "Anonymous 1");
+    }
+    public Boolean assertEquals(String actual, String expected) {
+        return (actual.equals(expected));
+    }
+
+    public Boolean assertEquals(Vector<String> actual, String expected) {
+        for(int i=0; i<actual.size(); i++) {
+            if(!actual.get(i).equals(expected)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Boolean assertEquals(Post post) {
+        return (post == null);
+    }
+}
